4b81d856526614fbf1a72c4a6717118b9f565e13
Andrew Rosen <asrosen93@gmail.com>
2022-04-15 21:24:21 +0000

    Update ase/test/calculator/dftb/test_xtb_static.py

--- ase/test/calculator/dftb/test_xtb_static.py
+++ ase/test/calculator/dftb/test_xtb_static.py
@@ -5,10 +5,9 @@ from ase.calculators.dftb import Dftb
 
 @pytest.mark.calculator_lite
 @pytest.mark.calculator('dftb')
-def test_xtb_static(dftb_factory):
+def test_xtb_static(factory):
     atoms = molecule('H2O')
-    calc = Dftb(atoms=atoms, Hamiltonian_='xTB', Hamiltonian_Method='GFN2-xTB')
-    atoms.calc = calc
+    atoms.calc = factory.calc(atoms=atoms, Hamiltonian_='xTB', Hamiltonian_Method='GFN2-xTB')
 
     e = atoms.get_potential_energy()
     assert e == pytest.approx(-137.97459675495645)
