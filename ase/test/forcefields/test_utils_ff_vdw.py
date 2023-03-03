a947da22344cf2292321f0409efc9a87052d02b0
Peter Koval <koval.peter@gmail.com>
2023-03-03 20:28:18 +0100

    reformatting

--- ase/test/forcefields/test_utils_ff_vdw.py
+++ ase/test/forcefields/test_utils_ff_vdw.py
@@ -8,7 +8,7 @@ from ase.utils.ff import VdW, get_vdw_potential_value
 
 @pytest.fixture
 def atoms() -> Atoms:
-    mol = molecule('H2')
+    mol = molecule("H2")
     mol.center(5.0)
     return mol
 
@@ -16,7 +16,14 @@ def atoms() -> Atoms:
 def test_utils_ff_vdw(atoms: Atoms) -> None:
     """Check the equivalent van der Waals energies."""
     e1, e2, s1, s2, scale = 0.646, 1.557, 0.777, 0.724, 1.2236
-    vdw_1 = VdW(0, 1, epsiloni=e1, epsilonj=e2, sigmai=s1, sigmaj=s2, scale=scale)
-    vdw_2 = VdW(0, 1, epsilonij=math.sqrt(e1 * e2), sigmaij=(s1 + s2) / 2.0, scale=scale)
-    e_ref = get_vdw_potential_value(atoms, vdw_1)
-    assert get_vdw_potential_value(atoms, vdw_2) == pytest.approx(e_ref)
+
+    vdw_a = VdW(
+        0, 1, epsiloni=e1, epsilonj=e2, sigmai=s1, sigmaj=s2, scale=scale
+    )
+
+    vdw_p = VdW(
+        0, 1, epsilonij=math.sqrt(e1 * e2), sigmaij=(s1 + s2) / 2.0, scale=scale
+    )
+
+    e_ref = get_vdw_potential_value(atoms, vdw_a)
+    assert get_vdw_potential_value(atoms, vdw_p) == pytest.approx(e_ref)
