3a6ac6e2e20bebfc2c9ea510f563f2d8eb8425d3
Peter Koval <koval.peter@gmail.com>
2023-03-15 22:52:21 +0100

    The fix.

--- ase/io/sdf.py
+++ ase/io/sdf.py
@@ -2,21 +2,24 @@
 
 See https://en.wikipedia.org/wiki/Chemical_table_file#SDF
 """
+from typing import TextIO
+
 from ase.atoms import Atoms
 from ase.utils import reader
 
 
 @reader
-def read_sdf(fileobj):
-    lines = fileobj.readlines()
+def read_sdf(file_obj: TextIO) -> Atoms:
+    lines = file_obj.readlines()
     # first three lines header
     del lines[:3]
 
-    L1 = lines.pop(0).split()
-    natoms = int(L1[0])
+    first_line = lines.pop(0)
+    num_atoms = int(first_line[0:3])  # first three characters
+    # http://biotech.fyicenter.com/1000024_SDF_File_Format_Specification.html
     positions = []
     symbols = []
-    for line in lines[:natoms]:
+    for line in lines[:num_atoms]:
         x, y, z, symbol = line.split()[:4]
         symbols.append(symbol)
         positions.append([float(x), float(y), float(z)])
