6030673313fb344c8cd3253aa4d7df65ab0b32ac
Peter Koval <koval.peter@gmail.com>
2023-03-16 18:40:29 +0100

    More work in SDF

--- ase/io/sdf.py
+++ ase/io/sdf.py
@@ -8,19 +8,25 @@ from ase.atoms import Atoms
 from ase.utils import reader
 
 
+def get_num_atoms_sdf_v2000(first_line: str) -> int:
+    """Parse the first line extracting the number of atoms."""
+    return int(first_line[0:3])  # first three characters
+    # http://biotech.fyicenter.com/1000024_SDF_File_Format_Specification.html
+
+
 @reader
 def read_sdf(file_obj: TextIO) -> Atoms:
+    """Read the sdf data from a text file and compose the corresponding Atoms object."""
     lines = file_obj.readlines()
     # first three lines header
     del lines[:3]
 
-    first_line = lines.pop(0)
-    num_atoms = int(first_line[0:3])  # first three characters
-    # http://biotech.fyicenter.com/1000024_SDF_File_Format_Specification.html
+    num_atoms = get_num_atoms_sdf_v2000(lines.pop(0))
     positions = []
     symbols = []
     for line in lines[:num_atoms]:
-        x, y, z, symbol = line.split()[:4]
-        symbols.append(symbol)
-        positions.append([float(x), float(y), float(z)])
+        ls = line.split()
+        positions.append(tuple(map(float, ls[:3])))
+        symbols.append(ls[3])
+
     return Atoms(symbols=symbols, positions=positions)
