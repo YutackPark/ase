fb6734ae859a214afd8ea639cb96816e0ef84ccb
Steen Lysgaard <stly@dtu.dk>
2023-04-26 12:38:19 +0200

    more thorough test of incar writer

--- ase/test/calculator/vasp/test_vasp_incar.py
+++ ase/test/calculator/vasp/test_vasp_incar.py
@@ -8,7 +8,19 @@ calc = pytest.mark.calculator
 @calc('vasp')
 def test_vasp_incar(factory):
     """Test that INCAR is written correctly."""
-    settings = {'xc': 'PBE', 'encut': 400, 'ediff': 1e-6, 'ibrion': 2, 'prec': 'Low'}
+    # Have each of every type of key in settings
+    settings = {'xc': 'PBE',  # special str key
+                'encut': 400,  # Float key
+                'ediff': 1e-6,  # Exp key
+                'ibrion': 2,  # Int key
+                'prec': 'Low',  # str key
+                'lattice_constraints': [False, True, False],  # list_bool key
+                'iband': [1, 2, 3],  # list_int key
+                'lhfcalc': True,  # bool key
+                'lreal': True,  # special key
+                'magmom': [.5, 1.5],  # list_float key
+                'ldau_luj': {'H': {'L': 2, 'U': 4.0, 'J': 0.0},}  # dict key
+                }
     calc = factory.calc(**settings)
     atoms = Atoms('H2', positions=[[0, 0, 0], [0, 0, 1.2]])
     calc.initialize(atoms)
