3a609a9876a0f9b9908718204faff6b622b0ebc9
Andrew Rosen <asrosen93@gmail.com>
2023-06-17 00:10:45 -0700

    Clarify code comment

--- ase/thermochemistry.py
+++ ase/thermochemistry.py
@@ -204,8 +204,9 @@ class HinderedThermo(ThermoChem):
         number of 1 but propane bound through its middle carbon has a symmetry
         number of 2. (if symmetrynumber is unspecified, then the default is 1)
     ignore_imag_modes : bool
-        If True, imaginary frequencies will be ignored. If False (default),
-        an error will be raised if imaginary frequencies are present.
+        If True, imaginary frequencies present after the 3N-3 cut will be ignored.
+        If False (default), an error will be raised if imaginary frequencies are
+        present.
     """
 
     def __init__(self, vib_energies, trans_barrier_energy, rot_barrier_energy,
@@ -449,8 +450,9 @@ class IdealGasThermo(ThermoChem):
         are paired, 0.5 for a free radical with a single unpaired electron,
         1.0 for a triplet with two unpaired electrons, such as O_2.)
     ignore_imag_modes : bool
-        If True, imaginary frequencies will be ignored. If False (default),
-        an error will be raised if imaginary frequencies are present.
+        If True, imaginary frequencies present after the 3N-5/3N-6 cut will
+        be ignored. If False (default), a ValueError will be raised if
+        imaginary frequencies are present.
     """
 
     def __init__(self, vib_energies, geometry, potentialenergy=0.,
