6af5fafad015cc37a2adfe14f560f88d67871b22
Andrew Rosen <asrosen93@gmail.com>
2023-07-08 23:34:43 -0700

    lint

--- ase/calculators/orca.py
+++ ase/calculators/orca.py
@@ -1,7 +1,8 @@
 import re
 
 import ase.io.orca as io
-from ase.calculators.genericfileio import CalculatorTemplate, GenericFileIOCalculator
+from ase.calculators.genericfileio import (CalculatorTemplate,
+                                           GenericFileIOCalculator)
 
 
 def get_version_from_orca_header(orca_header):
