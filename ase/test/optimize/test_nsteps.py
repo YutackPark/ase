32471cbc8d0956e199380ecaf12a4519603b9325
Ask Hjorth Larsen <asklarsen@gmail.com>
2023-07-10 21:20:25 +0200

    test that optimizer performs specified number of steps (plus two, apparently)

---
+++ ase/test/optimize/test_nsteps.py
@@ -0,0 +1,27 @@
+import pytest
+
+from ase.optimize import BFGS
+from ase.calculators.emt import EMT
+
+
+@pytest.fixture
+def opt():
+    from ase.build import bulk
+    atoms = bulk('Au', cubic=True)
+    atoms.rattle(stdev=0.12345, seed=42)
+    atoms.calc = EMT()
+    return BFGS(atoms)
+
+
+# XXX Fails with steps=0.
+# It is also a little bit questionable whether there should be
+# three steps when we set steps=1.
+@pytest.mark.parametrize('steps', [1, 4])
+def test_nsteps(opt, steps):
+    irun = opt.irun(fmax=0, steps=steps)
+
+    for i in range(steps + 2):
+        next(irun)
+
+    with pytest.raises(StopIteration):
+        next(irun)
