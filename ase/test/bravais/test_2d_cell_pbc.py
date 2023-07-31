f7c852e3d9c9bdbd92903d156a39b0c9ad192b3d
Ask Hjorth Larsen <asklarsen@gmail.com>
2023-07-31 16:52:01 +0200

    add unit test of 2d cell handedness for HEX2D

--- ase/test/bravais/test_2d_cell_pbc.py
+++ ase/test/bravais/test_2d_cell_pbc.py
@@ -1,5 +1,7 @@
 import pytest
+import numpy as np
 from ase.cell import Cell
+from ase.lattice import identify_lattice
 
 
 @pytest.fixture
@@ -33,3 +35,21 @@ def test_mcl_obl(cell):
     print(path2d)
     assert path2d.cell.rank == 2
     assert path2d.cell.get_bravais_lattice().name == 'OBL'
+
+
+
+@pytest.mark.parametrize('angle', [60, 120])
+def test_2d_bandpath_handedness(angle):
+    """Test that the x/y part is right-handed in 2D lattice.
+
+    During lattice determination, the whole 3x3 matrix is right-handed
+    including "dummy" z axis in 2D cells.  However this did not
+    guarantee that the x/y part itself was right-handed, and this
+    test would fail for angle=60."""
+
+    cell = Cell.new([1, 1, 0, 90, 90, angle])
+    assert cell.get_bravais_lattice().name == 'HEX2D'
+    bandpath = cell.bandpath()
+
+    assert bandpath.cell.rank == 2
+    assert np.linalg.det(bandpath.cell[:2, :2]) > 0
