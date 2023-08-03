d09a7717454b1f0290341dc0292d239feea0476e
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2023-08-03 04:07:59 +0200

    Add test for `_read_magnetic_moments`

--- ase/test/calculator/vasp/test_vasp_calculator.py
+++ ase/test/calculator/vasp/test_vasp_calculator.py
@@ -2,8 +2,10 @@
 
 import os
 import sys
-import pytest
 
+import numpy as np
+import pytest
+from ase import Atoms
 from ase.build import molecule
 from ase.calculators.calculator import (CalculatorSetupError,
                                         get_calculator_class)
@@ -126,6 +128,52 @@ def test_spinpol_vs_ispin():
     calc = Vasp()
     calc._set_spinpol(atoms)
     assert calc.spinpol
+
+
+class TestReadMagneticMoments:
+    """Test if the "magnetization (x)" block in OUTCAR is parsed correctly."""
+
+    # from `bulk("Fe", "bcc", a=2.8630354989499160, cubic=True)`
+    lines_spd = [
+        " magnetization (x)\n",
+        "\n",
+        "# of ion       s       p       d       tot\n",
+        "------------------------------------------\n",
+        "    1       -0.009  -0.045   2.369   2.315\n",
+        "    2       -0.009  -0.045   2.369   2.315\n",
+        "--------------------------------------------------\n"
+        "tot         -0.019  -0.089   4.738   4.630\n",
+    ]
+
+    # from `bulk("Ga", "bcc", a=4.0692361730014719, cubic=True)`
+    lines_spdf = [
+        " magnetization (x)\n",
+        "\n",
+        "# of ion       s       p       d       f       tot\n",
+        "--------------------------------------------------\n",
+        "    1        0.013   0.007   0.329   6.877   7.227\n",
+        "    2        0.013   0.007   0.329   6.877   7.227\n",
+        "--------------------------------------------------\n",
+        "tot          0.026   0.014   0.659  13.755  14.454\n",
+    ]
+
+    @pytest.mark.parametrize(
+        'lines, magmoms_ref', (
+            (lines_spd, (2.315, 2.315)),
+            (lines_spdf, (7.227, 7.227)),
+        )
+    )
+    def test(self, lines, magmoms_ref):
+        """Test"""
+        calc = Vasp()
+        # dummy atoms
+        calc.atoms = Atoms(
+            ("Fe", "Gd"),
+            positions=((0.0, 0.0, 0.0), (0.5, 0.5, 0.5)),
+        )
+        calc.resort = [0, 1]
+        magmoms = calc._read_magnetic_moments(lines)
+        np.testing.assert_allclose(magmoms, magmoms_ref)
 
 
 def test_read_magnetic_moment():
