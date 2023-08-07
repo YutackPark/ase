148c40f6dee668fa927ab80ada3ddb29b3a81241
Ask Hjorth Larsen <asklarsen@gmail.com>
2023-08-07 19:45:56 +0200

    test H2 bond with socketio abinit, espresso and nwchem

---
+++ ase/test/calculator/socketio/test_h2_stiffness.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+
+from ase.build import molecule
+import numpy as np
+import pytest
+
+from ase import Atoms
+from ase.calculators.socketio import SocketIOCalculator
+
+
+def run(calc):
+    atoms = molecule('H2', vacuum=3.0)
+    atoms.center(vacuum=4.0, axis=2)
+    atoms.calc = calc
+
+    d0 = atoms.get_distance(0, 1)
+
+    energies = []
+    distances = []
+
+    # Coarse binding curve:
+    for factor in [0.95, 1.0, 1.05]:
+        dist = d0 * factor
+        atoms.positions[:, 2] = [-0.5 * dist, 0.5 * dist]
+        atoms.center()
+
+        energy = atoms.get_potential_energy()
+        energies.append(energy)
+        distances.append(dist)
+
+    # We use bad/inconsistent parameters, but the minimum will
+    # still be somewhere around 0.7
+    poly = np.polyfit(distances, energies, deg=2)
+    dmin = -0.5 * poly[1] / poly[0]
+
+    assert dmin == pytest.approx(0.766, abs=0.03)  # bond length
+    assert poly[0] == pytest.approx(20.0, abs=1.5)  # bond stiffness
+
+
+@pytest.mark.filterwarnings('ignore:Subprocess exited')
+@pytest.mark.calculator_lite
+@pytest.mark.calculator('abinit')
+@pytest.mark.calculator('espresso', ecutwfc=30)
+@pytest.mark.calculator('nwchem')
+def test_socketio_h2(factory):
+    with factory.socketio(unixsocket=f'ase_test_h2_{factory.name}') as calc:
+        run(calc)
