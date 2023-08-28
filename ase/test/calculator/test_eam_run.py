f4bbfd59f8b20bd62ffaaf321c4bee59a2370828
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2023-08-28 21:43:55 +0200

    Add test for reading EAM potentials

--- ase/test/calculator/test_eam_run.py
+++ ase/test/calculator/test_eam_run.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pytest
 
-from ase.build import fcc111
+from ase.build import bulk, fcc111
 
 
 @pytest.mark.calculator('eam')
@@ -14,3 +14,23 @@ def test_eam_run(factory):
 
     assert abs(-164.277599313 - slab.get_potential_energy()) < 1E-8
     assert abs(6.36379627645 - np.linalg.norm(slab.get_forces())) < 1E-8
+
+
+@pytest.mark.parametrize(
+    'potential,element',
+    (
+        ('Pt_u3.eam', 'Pt'),
+        ('NiAlH_jea.eam.alloy', 'Ni'),
+        ('NiAlH_jea.eam.fs', 'Ni'),
+        ('AlCu.adp', 'Al'),
+    )
+)
+@pytest.mark.calculator('eam')
+@pytest.mark.calculator_lite
+def test_read_potential(factory, potential: str, element: str):
+    """Test if the potential can be read without errors."""
+    potential = f'{factory.factory.potentials_path}/{potential}'
+    calc = factory.calc(potential=potential, elements=[element])
+    atoms = bulk(element)
+    atoms.calc = calc
+    atoms.get_potential_energy()
