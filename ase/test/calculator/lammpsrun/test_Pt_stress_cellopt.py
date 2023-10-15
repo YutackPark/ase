BUG: fix cell gradient in `ExpCellFilter`

ase/test/calculator/lammpsrun/test_Pt_stress_cellopt.py
@@ -2,7 +2,7 @@ import numpy as np
 from numpy.testing import assert_allclose
 import pytest
 from ase.build import bulk
-from ase.filters import ExpCellFilter
+from ase.filters import FrechetCellFilter
 from ase.optimize import BFGS
 
 
@@ -26,20 +26,18 @@ def test_Pt_stress_cellopt(factory):
                         calc.calculate_numerical_stress(atoms),
                         atol=1e-4, rtol=1e-4)
 
-        with BFGS(ExpCellFilter(atoms)) as opt:
+        with BFGS(FrechetCellFilter(atoms)) as opt:
             for i, _ in enumerate(opt.irun(fmax=0.001)):
                 pass
 
-        cell1_ref = np.array(
-            [[0.16524, 3.8999, 3.92855],
-             [4.211015, 0.634928, 5.047811],
-             [4.429529, 3.293805, 0.447377]]
-        )
+        cell1_ref = np.array([
+            [0.178351, 3.885347, 3.942046],
+            [4.19978, 0.591071, 5.062568],
+            [4.449044, 3.264038, 0.471548],
+        ])
 
         assert_allclose(np.asarray(atoms.cell), cell1_ref,
                         atol=3e-4, rtol=3e-4)
         assert_allclose(atoms.get_stress(),
                         calc.calculate_numerical_stress(atoms),
                         atol=1e-4, rtol=1e-4)
-
-        assert i < 80, 'Expected 59 iterations, got many more: {}'.format(i)

ase/test/calculator/socketio/test_python_interface.py
@@ -10,7 +10,7 @@ from ase.calculators.emt import EMT
 @pytest.mark.skipif(os.name != 'posix', reason='only posix')
 def test_socketio_python():
     from ase.build import bulk
-    from ase.filters import ExpCellFilter
+    from ase.filters import FrechetCellFilter
     from ase.optimize import BFGS
 
     atoms = bulk('Au') * (2, 2, 2)
@@ -23,7 +23,9 @@ def test_socketio_python():
     pid = os.getpid()
     with SocketIOCalculator(launch_client=client,
                             unixsocket=f'ase-python-{pid}') as atoms.calc:
-        with BFGS(ExpCellFilter(atoms)) as opt:
+        with BFGS(FrechetCellFilter(atoms)) as opt:
             opt.run(fmax=fmax)
-    forces = atoms.get_forces()
-    assert np.linalg.norm(forces, axis=0).max() < fmax
+
+    residual_forces = np.linalg.norm(atoms.get_forces(), axis=1)
+    assert len(residual_forces) == len(atoms)
+    assert np.max(residual_forces) < fmax

ase/test/precon/test_unitcellfilter.py
@@ -1,31 +1,30 @@
-import numpy as np
 import pytest
 
+import ase
 from ase.build import bulk
 from ase.calculators.lj import LennardJones
 from ase.optimize.precon import PreconLBFGS, Exp
-from ase.filters import UnitCellFilter, ExpCellFilter
+from ase.filters import UnitCellFilter, FrechetCellFilter, Filter
 
 
 # @pytest.mark.skip('FAILS WITH PYAMG')
 @pytest.mark.slow
-def test_precon():
-    cu0 = bulk("Cu") * (2, 2, 2)
+# Ignore UserWarning by failure of Armijo line search in PreconLBFGS
+@pytest.mark.filterwarnings('ignore::UserWarning')
+@pytest.mark.parametrize("filter_cls, tol", [
+    (UnitCellFilter, 1e-3),
+    # FrechetCellFilter allows relaxing to lower tolerance
+    (FrechetCellFilter, 1e-7),
+])
+def test_precon(filter_cls, tol: float):
+    cu0: ase.Atoms = bulk("Cu") * (2, 2, 2)
     lj = LennardJones(sigma=cu0.get_distance(0, 1))
 
+    ratio = 1.2
     cu = cu0.copy()
-    cu.set_cell(1.2 * cu.get_cell())
+    cu.set_cell(ratio * cu.get_cell())
     cu.calc = lj
-    ucf = UnitCellFilter(cu, constant_volume=True)
-    opt = PreconLBFGS(ucf, precon=Exp(mu=1.0, mu_c=1.0))
+    filter: Filter = filter_cls(cu, constant_volume=True)
+    opt = PreconLBFGS(filter, precon=Exp(mu=1.0, mu_c=1.0))
     opt.run(fmax=1e-3)
-    assert abs(np.linalg.det(cu.cell) / np.linalg.det(cu0.cell) - 1.2**3) < 1e-3
-
-    # EcpCellFilter allows relaxing to lower tolerance
-    cu = cu0.copy()
-    cu.set_cell(1.2 * cu.get_cell())
-    cu.calc = lj
-    ecf = ExpCellFilter(cu, constant_volume=True)
-    opt = PreconLBFGS(ecf, precon=Exp(mu=1.0, mu_c=1.0))
-    opt.run(fmax=1e-3)
-    assert abs(np.linalg.det(cu.cell) / np.linalg.det(cu0.cell) - 1.2**3) < 1e-7
+    assert abs(cu.get_volume() / cu0.get_volume() - ratio**3) < tol