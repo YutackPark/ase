512cc2ef1dd1a7bedff32ccd253a0761bc50885b
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2023-10-25 01:27:13 +0200

    Remove unnecessary raise OptimizerConvergenceError
    An `Optimizer` class should *not* stop even if forces are not converged
    after the given steps. Convergence check will be done afterwords by the
    `converged` method in `Optimizer`.

--- ase/optimize/ode.py
+++ ase/optimize/ode.py
@@ -174,9 +174,6 @@ def ode12r(f, X0, h=None, verbose=1, fmax=1e-6, maxtol=1e3, steps=100,
             raise OptimizerConvergenceError('ODE12r terminates unsuccessfully'
                                             f' Step size {h} too small')
 
-    raise OptimizerConvergenceError(f'ODE12r terminates unsuccessfully after '
-                                    f'{steps} iterations.')
-
 
 class ODE12r(SciPyOptimizer):
     """
