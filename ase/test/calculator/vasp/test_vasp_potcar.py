73ccf858fab5af13949e5a950051bf8a716647af
Michael Wolloch <michael.wolloch@univie.ac.at>
2023-10-31 17:29:41 +0100

    Made a test for potential mapping with the 'setups' parameter. Still needs to be optimized to run only if POTCARs are actually available.

--- ase/test/calculator/vasp/test_vasp_potcar.py
+++ ase/test/calculator/vasp/test_vasp_potcar.py
@@ -3,37 +3,44 @@ import pytest
 from unittest import mock
 
 import numpy as np
-from ase.calculators.vasp.create_input import GenerateVaspInput
+from ase.calculators.vasp import Vasp
 from ase.calculators.vasp.create_input import _args_without_comment
 from ase.calculators.vasp.create_input import _to_vasp_bool, _from_vasp_bool
 
 from ase.build import bulk
 
-calc = pytest.mark.calculator
 @pytest.fixture
 def nacl():
     atoms = bulk('NaCl', crystalstructure='rocksalt', a=4.1,
-                 cubic=True) * (3, 3, 3)
+                 cubic=True)
     return atoms
 
 
-@pytest.fixture
-def vaspinput_factory(nacl):
-    """Factory for GenerateVaspInput class, which mocks the generation of
-    pseudopotentials."""
-    def _vaspinput_factory(atoms=None, **kwargs) -> GenerateVaspInput:
-        if atoms is None:
-            atoms = nacl
-        mocker = mock.Mock()
-        inputs = GenerateVaspInput()
-        inputs.set(**kwargs)
-        inputs._build_pp_list = mocker(return_value=None)  # type: ignore
-        inputs.initialize(atoms)
-        return inputs
 
-    return _vaspinput_factory
-
-@calc('vasp')
 def test_vasp_potcar(nacl):
-    vaspinputs = GenerateVaspInput(setups='recommended')
-    assert vaspinputs.ppp_list == ['Na_pv', 'Cl']
+    def get_pp_symbols(ppp_list):
+        mappings = {}
+        for p in ppp_list:
+            name = (p.split('/')[-2])
+            element = name.split('_')[0] if '.' not in name else 'H'
+            mappings[element] = name
+        return mappings
+
+    setups = {'recommended':
+                  {'Na': 'Na_pv', 'Cl': 'Cl'},
+              'GW':
+                    {'Na': 'Na_sv_GW', 'Cl': 'Cl_GW'},
+              'custom':
+                    {'Na': 'Na', 'Cl': 'Cl_h'},
+              }
+    calc = Vasp(setups='recommended')
+    calc.initialize(nacl)
+    assert get_pp_symbols(calc.ppp_list) == setups['recommended']
+
+    calc = Vasp(setups='GW')
+    calc.initialize(nacl)
+    assert get_pp_symbols(calc.ppp_list) == setups['GW']
+
+    calc = Vasp(setups={'base': 'minimal', 'Cl': '_h'})
+    calc.initialize(nacl)
+    assert get_pp_symbols(calc.ppp_list) == setups['custom']
