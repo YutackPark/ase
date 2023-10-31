9f3e968ec37cbb214438565e53fa6b08f0176ef8
Mikael Kuisma <kuisma@dtu.dk>
2023-10-31 10:49:57 +0100

    add first test, create cellawarebfgs class

--- ase/optimize/cellawarebfgs.py
+++ ase/optimize/cellawarebfgs.py
@@ -1,6 +1,85 @@
+from typing import IO, Optional, Union
+import numpy as np
 from ase.filters import ExpCellFilter
 from ase.optimize import BFGS
-import numpy as np
+from ase import Atoms
+
+
+def calculate_isotropic_elasticity_tensor(bulk_modulus, poisson_ratio, suppress_rotation=0):
+    """
+    Parameters:
+           bulk_modulus  Bulk Modulus of the isotropic system used to set up
+                         the Hessian (in GPa). Default value 500 GPa corresponds
+                         approximately to diamond.
+
+           poisson_ratio Poisson ratio of the isotropic system ysed to set up
+                         the initial Hessian (unitless, between -1 and 0.5).
+                         Default value is 0.3.
+
+           suppress_rotation The rank-2 matrix C_ijkl.reshape((9,9)) has by default
+                             6 non zero eigenvalues, because energy is invariant 
+                             to ortonormal rotations of the cell vector.
+                             This serves as a bad initial Hessian due to 3 zero eigenvalues.
+                             Suppress rotation sets a value for those zero eigenvalues.
+
+           Returns C_ijkl
+    """
+
+    bulk_modulus = 0.00624150907 * bulk_modulus # GPa to eV / Å^3
+    g = np.eye(3)
+
+    # https://scienceworld.wolfram.com/physics/LameConstants.html
+    _lambda = 3 * bulk_modulus * (poisson_ratio) / (1 + 1 * poisson_ratio)
+    _mu = _lambda * (1 - 2 * poisson_ratio) / (2 * poisson_ratio)
+
+    # https://en.wikipedia.org/wiki/Elasticity_tensor
+    g_ij = np.eye(3)
+
+    # Construct 4th rank Elasticity tensor for isotropic systems
+    C_ijkl = _lambda * np.einsum('ij,kl->ijkl',g_ij,g_ij)
+    C_ijkl += _mu * (np.einsum('ik,jl->ijkl', g_ij, g_ij) +
+                     np.einsum('il,kj->ijkl', g_ij, g_ij))
+
+    # Supplement the tensor with suppression of pure rotations (which are right now 0 eigenvalues)
+    # Loop over all basis vectors of skew symmetric real matrix
+    for i,j in ((0,1),(0,2),(1,2)):
+        Q = np.zeros((3,3))
+        Q[i,j], Q[j,i] = 1, -1
+        C_ijkl += np.einsum('ij,kl->ijkl', Q, Q) * suppress_rotation / 2
+
+    return C_ijkl
+
+
+class CellAwareBFGS(BFGS):
+    def __init__(
+        self,
+        atoms: Atoms,
+        restart: Optional[str] = None,
+        logfile: Union[IO, str] = '-',
+        trajectory: Optional[str] = None,
+        maxstep: Optional[float] = None,
+        maxcellstep: Optional[float] = None,
+        master: Optional[bool] = None,
+        bulk_modulus: Optional[float] = 145,
+        poisson_ratio: Optional[float] = 0.3,
+        alpha: Optional[float] = None,
+    ):
+        self.bulk_modulus = bulk_modulus
+        self.poisson_ratio = poisson_ratio
+        BFGS.__init__(self, atoms, restart, logfile,
+                      trajectory,
+                      maxstep, master, alpha)
+        assert not isinstance(atoms, Atoms)
+    
+    def initialize(self):
+        BFGS.initialize(self)
+        C_ijkl = calculate_isotropic_elasticity_tensor(
+            self.bulk_modulus,
+            self.poisson_ratio,
+            suppress_rotation=self.alpha
+            )
+        self.H0[-9:, -9:] = C_ijkl.reshape((9,9)) * self.atoms.atoms.cell.volume
+
 
 def ExactHessianBFGS(atoms, C_ijkl, alpha=70):
     atoms_and_cell = ExpCellFilter(atoms)
---
+++ ase/test/optimize/test_cellawarebfgs.py
@@ -0,0 +1,98 @@
+from ase.optimize.cellawarebfgs import CellAwareBFGS
+from ase.filters import FrechetCellFilter
+from ase.build import bulk
+from ase.calculators.emt import EMT
+
+
+def test_cellaware_bfgs():
+    atoms = bulk('Au')
+    atoms *= 5
+    atoms.calc = EMT()
+    relax = CellAwareBFGS(FrechetCellFilter(atoms), alpha=70)
+    relax.run()
+     
+
+def oldcode():
+    from ase.stress import get_elasticity_tensor
+    import numpy as np
+    from typing import Tuple
+
+    from ase import Atoms
+    from ase.build import bulk
+    from ase.filters import ExpCellFilter
+    from ase.optimize import BFGS
+    from ase.io import read
+
+    from run_optimization import run_optimization
+    from hessian_initial import ExactHessianBFGS, CellBFGS
+
+    from gpaw import GPAW, PW
+
+    from ase.calculators.emt import EMT
+    #def EMT():
+    #    return GPAW(mode=PW(ecut=400), kpts=(4,4,4), txt='gpaw.txt')
+
+
+    atoms = bulk('Au')
+    atoms *= 5
+    #atoms[0].symbol ='Al'
+    from sys import argv
+
+    alpha = 70
+
+    #atoms = read(argv[1])
+    #atoms.rattle(0.1)
+    np.random.seed(1)
+    atoms.calc = EMT()
+
+    #relax = BFGS(atoms)
+    #relax.run()
+
+    #relax = BFGS(ExpCellFilter(atoms), alpha=alpha, logfile='/dev/null')
+    relax = CellBFGS(atoms)
+    run_optimization(relax)
+    C_ijkl = get_elasticity_tensor(atoms)
+    for i in range(3):
+        for j in range(3):
+            print(f'C_ijkl[{i}, {j}] =')
+            for k in range(3):
+                for l in range(3):
+                    print(round(C_ijkl[i,j,k,l], 2), end=' ')
+                print()
+            print()
+        print()
+
+    d = 0.01
+    deformation = np.eye(3) + d*(np.random.rand(3,3)-0.5)
+    atoms.set_cell(atoms.get_cell() @ deformation, scale_atoms=True)
+
+    #atoms *= (1,2,3)
+
+
+
+    np.set_printoptions(precision=3, linewidth=200, suppress=True)
+
+    atoms_cellbfgs = atoms.copy()
+    atoms_cellbfgs.calc = EMT()
+    relax_cellbfgs = CellBFGS(atoms_cellbfgs, alpha=alpha)
+    print('Hessian initial guess tensor\n', np.round(relax_cellbfgs.H0[-9:, -9:],3))
+    run_optimization(relax_cellbfgs)
+    steps_cellbfgs = relax_cellbfgs.nsteps
+
+    atoms_exact = atoms.copy()
+    atoms_exact.calc = EMT()
+    relax_exact = ExactHessianBFGS(atoms_exact, C_ijkl, alpha=alpha)
+    print('Exact elasticity tensor\n', np.round(relax_exact.H0[-9:, -9:],3))
+    run_optimization(relax_exact)
+    steps_exact = relax_exact.nsteps
+
+    if 1:
+        atoms_copy = atoms.copy()
+        atoms_copy.calc = EMT()
+        relax = BFGS(ExpCellFilter(atoms_copy), logfile='/dev/null')
+        relax.masks = [ np.ones( 3 * len(atoms) + 9) ]
+        run_optimization(relax)
+        steps_naive = relax.nsteps
+
+    #run_optimization(relax)
+    print(f'Naive {steps_naive} CellBFGS {steps_cellbfgs} Exact cell hessian {steps_exact}') 
