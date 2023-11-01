f2610e5f1f31bd31708a15d7760cf264d67dc2ef
Michael Wolloch <michael.wolloch@univie.ac.at>
2023-11-01 14:00:54 +0100

    added FutureWarning for pw91 to the relevant test.

--- ase/test/calculator/vasp/test_vasp_input.py
+++ ase/test/calculator/vasp/test_vasp_input.py
@@ -223,14 +223,15 @@ def test_vasp_xc(vaspinput_factory):
     assert calc_hse.bool_params['lhfcalc'] is True
     assert dict_is_subset({'gga': 'RE'}, calc_hse.string_params)
 
-    calc_pw91 = vaspinput_factory(xc='pw91',
-                                  kpts=(2, 2, 2),
-                                  gamma=True,
-                                  lreal='Auto')
-    assert dict_is_subset(
-        {
-            'pp': 'PW91',
-            'kpts': (2, 2, 2),
-            'gamma': True,
-            'reciprocal': False
-        }, calc_pw91.input_params)
+    with pytest.warns(FutureWarning):
+        calc_pw91 = vaspinput_factory(xc='pw91',
+                                      kpts=(2, 2, 2),
+                                      gamma=True,
+                                      lreal='Auto')
+        assert dict_is_subset(
+            {
+                'pp': 'PW91',
+                'kpts': (2, 2, 2),
+                'gamma': True,
+                'reciprocal': False
+            }, calc_pw91.input_params)
