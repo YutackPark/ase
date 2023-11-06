57a654cea2506ac62baa1783574beddb41896253
Ask Hjorth Larsen <asklarsen@gmail.com>
2023-11-06 18:39:45 +0100

    more legacy commands

--- ase/calculators/qchem.py
+++ ase/calculators/qchem.py
@@ -11,7 +11,7 @@ class QChem(FileIOCalculator):
     name = 'QChem'
 
     implemented_properties = ['energy', 'forces']
-    command = 'qchem PREFIX.inp PREFIX.out'
+    _legacy_default_command = 'qchem PREFIX.inp PREFIX.out'
 
     # Following the minimal requirements given in
     # http://www.q-chem.com/qchem-website/manual/qchem43_manual/sect-METHOD.html
