ded9fbf16df30328cae8a5b41d599333b2b4583e
Alex Hoffman <ahoffman2011@gmail.com>
2023-11-30 17:37:56 +0000

    changed SumCalculator method of computing stresses

--- ase/test/test_mixingcalc.py
+++ ase/test/test_mixingcalc.py
@@ -4,9 +4,13 @@ import pytest
 from ase.build import fcc111
 from ase.calculators.calculator import CalculatorSetupError
 from ase.calculators.emt import EMT
-from ase.calculators.mixing import (AverageCalculator,
-                                    LinearCombinationCalculator,
-                                    MixedCalculator, SumCalculator)
+from ase.calculators.mixing import (
+    Mixer,
+    AverageCalculator,
+    LinearCombinationCalculator,
+    MixedCalculator,
+    SumCalculator,
+)
 from ase.constraints import FixAtoms
 
 
@@ -16,7 +20,7 @@ def test_mixingcalc():
     """
 
     # Calculate reference values:
-    atoms = fcc111('Cu', (2, 2, 1), vacuum=10.)
+    atoms = fcc111("Cu", (2, 2, 1), vacuum=10.0)
     atoms[0].x += 0.2
 
     # First run the test with EMT similarly to the test of the single point
@@ -24,6 +28,13 @@ def test_mixingcalc():
     calc = EMT()
     atoms.calc = calc
     forces = atoms.get_forces()
+    voigt_stress = atoms.get_stress()
+    stress_tensor = atoms.get_stress(voigt=False)
+
+    # Mixer: check that stresses of different shapes are handled correctly
+    stresses = [voigt_stress, stress_tensor]
+    reshape_stress = Mixer.make_stress_voigt(stresses)
+    assert np.isclose(reshape_stress[0], reshape_stress[1]).all()
 
     # SumCalculator: Only one way to associate a calculator with an atoms
     # object.
@@ -33,6 +44,8 @@ def test_mixingcalc():
 
     # Check the results.
     assert np.isclose(2 * forces, atoms1.get_forces()).all()
+    assert np.isclose(2 * voigt_stress, atoms1.get_stress()).all()
+    assert atoms1.get_stress().shape == (6,)
 
     # testing  step
     atoms1[0].x += 0.2
@@ -51,7 +64,7 @@ def test_mixingcalc():
     # LinearCombinationCalculator:
 
     atoms2 = atoms.copy()
-    calc2 = LinearCombinationCalculator([EMT(), EMT()], weights=[.5, .5])
+    calc2 = LinearCombinationCalculator([EMT(), EMT()], weights=[0.5, 0.5])
     atoms2.calc = calc2
 
     # Check the results (it should be the same because it is tha average of
