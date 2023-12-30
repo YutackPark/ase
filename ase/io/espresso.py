99eaf6575d25d842b282c4ff0b06c12977e1e40f
Andrew-S-Rosen <asrosen93@gmail.com>
2023-12-29 19:24:17 -0800

    Re-add now-deprecated Espresso IO functions

--- ase/io/espresso.py
+++ ase/io/espresso.py
@@ -16,20 +16,21 @@ import operator as op
 import re
 import warnings
 from collections import defaultdict
+from copy import deepcopy
+from pathlib import Path
 
 import numpy as np
 
 from ase.atoms import Atoms
 from ase.calculators.calculator import kpts2ndarray, kpts2sizeandoffsets
-from ase.calculators.singlepoint import (SinglePointDFTCalculator,
-                                         SinglePointKPoint)
+from ase.calculators.singlepoint import SinglePointDFTCalculator, SinglePointKPoint
 from ase.constraints import FixAtoms, FixCartesian
 from ase.data import chemical_symbols
 from ase.dft.kpoints import kpoint_convert
-from ase.units import create_units
-from ase.utils import reader, writer
-
 from ase.io.espresso_namelist.namelist import Namelist
+from ase.io.espresso_namelist.keys import pw_keys
+from ase.units import create_units
+from ase.utils import deprecated, reader, writer
 
 # Quantum ESPRESSO uses CODATA 2006 internally
 units = create_units('2006')
@@ -1893,3 +1894,154 @@ def write_fortran_namelist(
         fd.write(additional_fields)
 
     fd.write("EOF")
+
+@deprecated('Please use ase.io.espresso.write_fortran_namelist.',
+            DeprecationWarning)
+def construct_namelist(parameters=None, keys=None, warn=False, **kwargs):
+    """
+    Construct an ordered Namelist containing all the parameters given (as
+    a dictionary or kwargs). Keys will be inserted into their appropriate
+    section in the namelist and the dictionary may contain flat and nested
+    structures. Any kwargs that match input keys will be incorporated into
+    their correct section. All matches are case-insensitive, and returned
+    Namelist object is a case-insensitive dict.
+
+    If a key is not known to ase, but in a section within `parameters`,
+    it will be assumed that it was put there on purpose and included
+    in the output namelist. Anything not in a section will be ignored (set
+    `warn` to True to see ignored keys).
+
+    Keys with a dimension (e.g. Hubbard_U(1)) will be incorporated as-is
+    so the `i` should be made to match the output.
+
+    The priority of the keys is:
+        kwargs[key] > parameters[key] > parameters[section][key]
+    Only the highest priority item will be included.
+
+    .. deprecated:: 3.21.0
+        Please use :func:`ase.io.espresso.write_fortran_namelist` instead.
+
+    Parameters
+    ----------
+    parameters: dict
+        Flat or nested set of input parameters.
+    keys: Namelist | dict
+        Namelist to use as a template for the output.
+    warn: bool
+        Enable warnings for unused keys.
+
+    Returns
+    -------
+    input_namelist: Namelist
+        pw.x compatible namelist of input parameters.
+
+    """
+
+    if keys is None:
+        keys = deepcopy(pw_keys)
+    # Convert everything to Namelist early to make case-insensitive
+    if parameters is None:
+        parameters = Namelist()
+    else:
+        # Maximum one level of nested dict
+        # Don't modify in place
+        parameters_namelist = Namelist()
+        for key, value in parameters.items():
+            if isinstance(value, dict):
+                parameters_namelist[key] = Namelist(value)
+            else:
+                parameters_namelist[key] = value
+        parameters = parameters_namelist
+
+    # Just a dict
+    kwargs = Namelist(kwargs)
+
+    # Final parameter set
+    input_namelist = Namelist()
+
+    # Collect
+    for section in keys:
+        sec_list = Namelist()
+        for key in keys[section]:
+            # Check all three separately and pop them all so that
+            # we can check for missing values later
+            value = None
+
+            if key in parameters.get(section, {}):
+                value = parameters[section].pop(key)
+            if key in parameters:
+                value = parameters.pop(key)
+            if key in kwargs:
+                value = kwargs.pop(key)
+
+            if value is not None:
+                sec_list[key] = value
+
+            # Check if there is a key(i) version (no extra parsing)
+            for arg_key in list(parameters.get(section, {})):
+                if arg_key.split('(')[0].strip().lower() == key.lower():
+                    sec_list[arg_key] = parameters[section].pop(arg_key)
+            cp_parameters = parameters.copy()
+            for arg_key in cp_parameters:
+                if arg_key.split('(')[0].strip().lower() == key.lower():
+                    sec_list[arg_key] = parameters.pop(arg_key)
+            cp_kwargs = kwargs.copy()
+            for arg_key in cp_kwargs:
+                if arg_key.split('(')[0].strip().lower() == key.lower():
+                    sec_list[arg_key] = kwargs.pop(arg_key)
+
+        # Add to output
+        input_namelist[section] = sec_list
+
+    unused_keys = list(kwargs)
+    # pass anything else already in a section
+    for key, value in parameters.items():
+        if key in keys and isinstance(value, dict):
+            input_namelist[key].update(value)
+        elif isinstance(value, dict):
+            unused_keys.extend(list(value))
+        else:
+            unused_keys.append(key)
+
+    if warn and unused_keys:
+        warnings.warn('Unused keys: {}'.format(', '.join(unused_keys)))
+
+    return input_namelist
+
+@deprecated('Please use the .to_string() method of Namelist instead.',
+            DeprecationWarning)
+def namelist_to_string(input_parameters):
+    """Format a Namelist object as a string for writing to a file.
+    Assume sections are ordered (taken care of in namelist construction)
+    and that repr converts to a QE readable representation (except bools)
+
+    .. deprecated:: 3.21.0
+        Please use the :meth:`ase.io.espresso.Namelist.to_string` method
+        instead.
+
+    Parameters
+    ----------
+    input_parameters : Namelist | dict
+        Expecting a nested dictionary of sections and key-value data.
+
+    Returns
+    -------
+    pwi : List[str]
+        Input line for the namelist
+    """
+    pwi = []
+    for section in input_parameters:
+        pwi.append(f'&{section.upper()}\n')
+        for key, value in input_parameters[section].items():
+            if value is True:
+                pwi.append(f'   {key:16} = .true.\n')
+            elif value is False:
+                pwi.append(f'   {key:16} = .false.\n')
+            elif isinstance(value, Path):
+                pwi.append(f'   {key:16} = "{value}"\n')
+            else:
+                # repr format to get quotes around strings
+                pwi.append(f'   {key:16} = {value!r}\n')
+        pwi.append('/\n')  # terminate section
+    pwi.append('\n')
+    return pwi
