def3c602dcddd637795c0ccea5f2d9e9b1eab5e8
Ask Hjorth Larsen <asklarsen@gmail.com>
2024-01-10 22:35:43 +0200

    start moving standalone stuff to siesta io input module

--- ase/calculators/siesta/siesta.py
+++ ase/calculators/siesta/siesta.py
@@ -26,10 +26,11 @@ from ase.calculators.calculator import (FileIOCalculator, Parameters,
 from ase.calculators.siesta.import_functions import (get_valence_charge,
                                                      read_rho,
                                                      read_vca_synth_block)
-from ase.calculators.siesta.parameters import (PAOBasisBlock, Species,
-                                               format_fdf)
+from ase.calculators.siesta.parameters import PAOBasisBlock, format_fdf
 from ase.data import atomic_numbers
 from ase.io.siesta import read_siesta_xv
+from ase.io.siesta_input import (
+    get_species, write_kpts, make_xyz_constraints)
 from ase.units import Bohr, Ry, eV
 from ase.utils import deprecated
 
@@ -138,22 +139,6 @@ def resolve_band_structure(path, kpts, energies, efermi):
     skn2e = np.swapaxes(ksn2e, 0, 1)
     bs = BandStructure(path, skn2e, reference=efermi)
     return bs
-
-
-def is_along_cartesian(norm_dir: np.ndarray) -> bool:
-    """Return whether `norm_dir` is along a Cartesian coordidate."""
-    directions = [
-        [+1, 0, 0],
-        [-1, 0, 0],
-        [0, +1, 0],
-        [0, -1, 0],
-        [0, 0, +1],
-        [0, 0, -1],
-    ]
-    for direction in directions:
-        if np.allclose(norm_dir, direction, rtol=0.0, atol=1e-6):
-            return True
-    return False
 
 
 class SiestaParameters(Parameters):
@@ -1197,110 +1182,3 @@ class Siesta3_2(Siesta):
             'SpinOrbit' if needed.
         """
         Siesta.__init__(self, *args, **kwargs)
-
-
-def write_kpts(fd, kpts):
-    """Write kpts.
-
-    Parameters:
-        - f : Open filename.
-    """
-    fd.write('\n')
-    fd.write('#KPoint grid\n')
-    fd.write('%block kgrid_Monkhorst_Pack\n')
-
-    for i in range(3):
-        s = ''
-        if i < len(kpts):
-            number = kpts[i]
-            displace = 0.0
-        else:
-            number = 1
-            displace = 0
-        for j in range(3):
-            if j == i:
-                write_this = number
-            else:
-                write_this = 0
-            s += '     %d  ' % write_this
-        s += '%1.1f\n' % displace
-        fd.write(s)
-    fd.write('%endblock kgrid_Monkhorst_Pack\n')
-    fd.write('\n')
-
-
-def get_species(atoms, species, basis_set):
-    # For each element use default species from the species input, or set
-    # up a default species  from the general default parameters.
-    symbols = np.array(atoms.get_chemical_symbols())
-    tags = atoms.get_tags()
-    default_species = [
-        s for s in species
-        if (s['tag'] is None) and s['symbol'] in symbols]
-    default_symbols = [s['symbol'] for s in default_species]
-    for symbol in symbols:
-        if symbol not in default_symbols:
-            spec = Species(symbol=symbol,
-                           basis_set=basis_set,
-                           tag=None)
-            default_species.append(spec)
-            default_symbols.append(symbol)
-    assert len(default_species) == len(np.unique(symbols))
-
-    # Set default species as the first species.
-    species_numbers = np.zeros(len(atoms), int)
-    i = 1
-    for spec in default_species:
-        mask = symbols == spec['symbol']
-        species_numbers[mask] = i
-        i += 1
-
-    # Set up the non-default species.
-    non_default_species = [s for s in species if s['tag'] is not None]
-    for spec in non_default_species:
-        mask1 = (tags == spec['tag'])
-        mask2 = (symbols == spec['symbol'])
-        mask = np.logical_and(mask1, mask2)
-        if sum(mask) > 0:
-            species_numbers[mask] = i
-            i += 1
-    all_species = default_species + non_default_species
-
-    return all_species, species_numbers
-
-
-def make_xyz_constraints(atoms: Atoms):
-    """ Create coordinate-resolved list of constraints [natoms, 0:3]
-    The elements of the list must be integers 0 or 1
-      1 -- means that the coordinate will be updated during relaxation
-      0 -- mains that the coordinate will be fixed during relaxation
-    """
-    import sys
-
-    from ase.constraints import (FixAtoms, FixCartesian, FixedLine,
-                                 FixedPlane)
-
-    a2c = np.ones((len(atoms), 3), dtype=int)  # (0: fixed, 1: updated)
-    for c in atoms.constraints:
-        if isinstance(c, FixAtoms):
-            a2c[c.get_indices()] = 0
-        elif isinstance(c, FixedLine):
-            norm_dir = c.dir / np.linalg.norm(c.dir)
-            if not is_along_cartesian(norm_dir):
-                raise RuntimeError(
-                    'norm_dir: {} -- must be one of the Cartesian axes...'
-                    .format(norm_dir))
-            a2c[c.get_indices()] = norm_dir.round().astype(int)
-        elif isinstance(c, FixedPlane):
-            norm_dir = c.dir / np.linalg.norm(c.dir)
-            if not is_along_cartesian(norm_dir):
-                raise RuntimeError(
-                    'norm_dir: {} -- must be one of the Cartesian axes...'
-                    .format(norm_dir))
-            a2c[c.get_indices()] = abs(1 - norm_dir.round().astype(int))
-        elif isinstance(c, FixCartesian):
-            a2c[c.get_indices()] = c.mask.astype(int)
-        else:
-            warnings.warn('Constraint {} is ignored at {}'
-                          .format(str(c), sys._getframe().f_code))
-    return a2c
