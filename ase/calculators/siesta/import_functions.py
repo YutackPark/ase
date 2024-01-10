555f7626331de31bd68b38ca542c64f6e6ad513e
Ask Hjorth Larsen <asklarsen@gmail.com>
2024-01-10 22:49:14 +0200

    remove the read_vca_synth_block method which is now unused

--- ase/calculators/siesta/import_functions.py
+++ ase/calculators/siesta/import_functions.py
@@ -44,29 +44,6 @@ def read_rho(fname):
     return rho
 
 
-def read_vca_synth_block(filename, species_number=None):
-    """ Read the SyntheticAtoms block from the output of the
-    'fractional' siesta utility.
-
-    Parameters:
-        - filename: String with '.synth' output from fractional.
-        - species_number: Optional argument to replace override the
-                          species number in the text block.
-
-    Returns: A string that can be inserted into the main '.fdf-file'.
-    """
-    with open(filename) as fd:
-        lines = fd.readlines()
-    lines = lines[1:-1]
-
-    if species_number is not None:
-        lines[0] = '%d\n' % species_number
-
-    block = ''.join(lines).strip()
-
-    return block
-
-
 def readHSX(fname):
     """
     Read unformatted siesta HSX file
