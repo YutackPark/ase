410abde63a597c420998ae24d8fdd42b96d1373b
Rohit Goswami <rog32@hi.is>
2024-02-22 20:13:47 +0100

    ENH: Replace read_eon and rework states handles

--- ase/io/eon.py
+++ ase/io/eon.py
@@ -85,7 +85,10 @@ def make_atoms(coordblock, header):
     )
 
 
-def read_eon2(file_path, index=-1):
+def read_eon(fileobj, index=-1):
+    file_path = Path(fileobj)
+    if file_path.is_dir():
+        return read_states(file_path)
     images = []
     with open(file_path, "r") as fd:
         while True:
@@ -101,87 +104,21 @@ def read_eon2(file_path, index=-1):
             images.append(atoms)
 
     # XXX: I really don't like this..
+    # Since there should be a consistent return type
     if index == -1:
-        if len(images) == 1:
-            return images[0]
-        else:
-            return images
+        return images if len(images) > 1 else images[0]
     else:
         return images[index]
 
 
-def read_eon(fileobj, index=-1):
-    """Reads an EON reactant.con file.  If *fileobj* is the name of a
-    "states" directory created by EON, all the structures will be read."""
-    if isinstance(fileobj, str):
-        if os.path.isdir(fileobj):
-            return read_states(fileobj)
-        else:
-            fd = open(fileobj)
-    else:
-        fd = fileobj
-
-    more_images_to_read = True
-    images = []
-
-    first_line = fd.readline()
-    while more_images_to_read:
-
-        comment = first_line.strip()
-        fd.readline()  # 0.0000 TIME  (??)
-        cell_lengths = fd.readline().split()
-        cell_angles = fd.readline().split()
-        # Different order of angles in EON.
-        cell_angles = [cell_angles[2], cell_angles[1], cell_angles[0]]
-        cellpar = [float(x) for x in cell_lengths + cell_angles]
-        fd.readline()  # 0 0     (??)
-        fd.readline()  # 0 0 0   (??)
-        ntypes = int(fd.readline())  # number of atom types
-        natoms = [int(n) for n in fd.readline().split()]
-        atommasses = [float(m) for m in fd.readline().split()]
-
-        symbols = []
-        coords = []
-        masses = []
-        fixed = []
-        for n in range(ntypes):
-            symbol = fd.readline().strip()
-            symbols.extend([symbol] * natoms[n])
-            masses.extend([atommasses[n]] * natoms[n])
-            fd.readline()  # Coordinates of Component n
-            for i in range(natoms[n]):
-                row = fd.readline().split()
-                coords.append([float(x) for x in row[:3]])
-                fixed.append(bool(int(row[3])))
-
-        atoms = Atoms(symbols=symbols,
-                      positions=coords,
-                      masses=masses,
-                      cell=cellpar_to_cell(cellpar),
-                      constraint=FixAtoms(mask=fixed),
-                      info=dict(comment=comment))
-
-        images.append(atoms)
-
-        first_line = fd.readline()
-        if first_line == '':
-            more_images_to_read = False
-
-    if isinstance(fileobj, str):
-        fd.close()
-
-    if not index:
-        return images
-    else:
-        return images[index]
-
-
-def read_states(states_dir):
+def read_states(states_dir: Path):
     """Read structures stored by EON in the states directory *states_dir*."""
-    subdirs = glob(os.path.join(states_dir, '[0123456789]*'))
-    subdirs.sort(key=lambda d: int(os.path.basename(d)))
-    images = [read_eon(os.path.join(subdir, 'reactant.con'))
-              for subdir in subdirs]
+    subdirs = sorted([x for x in states_dir.iterdir() if x.is_dir() and x.name.isdigit()], key=lambda d: int(d.name))
+    images = []
+    for subdir in subdirs:
+        reactant_path = subdir / "reactant.con"
+        if reactant_path.exists():
+            images.extend(read_eon2(reactant_path, index=-1))
     return images
 
 
