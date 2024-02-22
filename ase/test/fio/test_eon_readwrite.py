2f513ccf24d674b66ed07335269c71c414550319
Rohit Goswami <rog32@hi.is>
2024-02-22 20:56:05 +0100

    TST: Expand and cleanup con file tests

--- ase/test/fio/test_eon_readwrite.py
+++ ase/test/fio/test_eon_readwrite.py
@@ -1,6 +1,7 @@
 """Check that reading and writing .con files is consistent."""
 
 import numpy as np
+import numpy.testing as npt
 
 import ase
 import ase.io
@@ -10,7 +11,7 @@ import ase.symbols
 TOL = 1e-6
 
 # The corresponding data as an ASE Atoms object.
-data = ase.Atoms(
+DATA = ase.Atoms(
     "Cu3",
     cell=np.array([[7.22, 0, 0], [1, 10.83, 0], [1, 1, 14.44]]),
     positions=np.array(
@@ -26,21 +27,26 @@ data = ase.Atoms(
 
 def test_eon_read_single(datadir):
     box = ase.io.read(f"{datadir}/io/con/single.con", format="eon")
-    # Check cell vectors.
-    assert (abs(box.cell - data.cell)).sum() < TOL  # read: cell vector check
-    # Check atom positions.
-    # read: position check
-    assert (abs(box.positions - data.positions)).sum() < TOL
+    npt.assert_allclose(box.cell, DATA.cell, rtol=TOL, atol=0)
     assert (box.symbols == ase.symbols.string2symbols("Cu3")).all() == True
-    assert abs(box.get_masses() - np.array([63.5459999] * 3)).sum() < TOL
+    npt.assert_allclose(box.get_masses(), np.array([63.5459999] * 3), rtol=TOL)
+    npt.assert_allclose(box.positions, DATA.positions, rtol=TOL)
+
 
 def test_eon_write_single(datadir):
     out_file = "out.con"
-    ase.io.write(out_file, data, format="eon")
+    ase.io.write(out_file, DATA, format="eon")
     data2 = ase.io.read(out_file, format="eon")
-    # Check cell vectors.
-    # write: cell vector check
-    assert (abs(data2.cell - data.cell)).sum() < TOL
-    # Check atom positions.
-    # write: position check
-    assert (abs(data2.positions - data.positions)).sum() < TOL
+    npt.assert_allclose(data2.cell, DATA.cell, rtol=TOL, atol=0)
+    npt.assert_allclose(data2.positions, DATA.positions, rtol=TOL)
+
+
+def test_eon_read_multi(datadir):
+    images = ase.io.read(f"{datadir}/io/con/multi.con", format="eon", index=":")
+    assert len(images) == 10
+    npt.assert_allclose(
+        images[0].constraints[0].get_indices(), np.array([0, 1]), rtol=1e-5, atol=0
+    )
+    npt.assert_allclose(
+        images[1].constraints[0].get_indices(), np.array([]), rtol=1e-5, atol=0
+    )
