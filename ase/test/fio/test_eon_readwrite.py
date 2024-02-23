6657fb22b735547ef06bd1e3f4c8343e1ca69f5a
Rohit Goswami <rog32@hi.is>
2024-02-23 12:51:41 +0100

    TST: Reduce test sharding

--- ase/test/fio/test_eon_readwrite.py
+++ ase/test/fio/test_eon_readwrite.py
@@ -76,3 +76,24 @@ def test_eon_isotope_fail():
     DATA.set_masses([33, 31, 22])
     with pytest.raises(RuntimeError):
         ase.io.write(out_file, DATA, format="eon")
+
+
+def test_eon_masses():
+    # Error tolerance.
+    TOL = 1e-8
+
+    data = ase.lattice.compounds.B2(['Cs', 'Cl'], latticeconstant=4.123,
+                                    size=(3, 3, 3))
+
+    m_Cs = ase.data.atomic_masses[ase.data.atomic_numbers['Cs']]
+    m_Cl = ase.data.atomic_masses[ase.data.atomic_numbers['Cl']]
+
+    con_file = 'pos.con'
+    # Write and read the .con file.
+    ase.io.write(con_file, data, format='eon')
+    data2 = ase.io.read(con_file, format='eon')
+    # Check masses.
+    symbols = np.asarray(data2.get_chemical_symbols())
+    masses = np.asarray(data2.get_masses())
+    assert (abs(masses[symbols == 'Cs'] - m_Cs)).sum() < TOL
+    assert (abs(masses[symbols == 'Cl'] - m_Cl)).sum() < TOL
