b427d58ec93a5745cf2d350c215b37b6f27a91c5
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2024-03-31 11:02:31 +0200

    Add `_legacy_default_command` and `fileio_rules`

--- ase/calculators/gaussian.py
+++ ase/calculators/gaussian.py
@@ -94,8 +94,13 @@ class GaussianIRC(GaussianDynamics):
 
 
 class Gaussian(FileIOCalculator):
+    _legacy_default_command = 'g16 < PREFIX.com > PREFIX.log'
     implemented_properties = ['energy', 'forces', 'dipole']
     discard_results_on_any_change = True
+
+    fileio_rules = FileIOCalculator.ruleset(
+        stdin_name='{prefix}.com',
+        stdout_name='{prefix}.log')
 
     def __init__(self, *args, label='Gaussian', **kwargs):
         super().__init__(*args, label=label, **kwargs)
