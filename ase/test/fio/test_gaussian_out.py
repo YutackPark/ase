513fdb0f83c50e58351be286cb59326f2a31fea0
Andrew-S-Rosen <asrosen93@gmail.com>
2024-04-09 22:45:51 -0700

    Revert "Merge branch 'gaussian-dipole' into 'master'"
    This reverts commit a1061f38cbb37669fc48d8c6482279d3194955da, reversing
    changes made to 0ca67f36ff4df1be8790742d005c8337021164f4.

--- ase/test/fio/test_gaussian_out.py
+++ ase/test/fio/test_gaussian_out.py
@@ -25,35 +25,26 @@ BUF_H2O = r"""
  Center     Atomic      Atomic             Coordinates (Angstroms)
  Number     Number       Type             X           Y           Z
  ---------------------------------------------------------------------
-      1          8           0        0.000000    0.000000    0.119262
-      2          1           0        0.000000    0.763239   -0.477047
-      3          1           0        0.000000   -0.763239   -0.477047
+      1          8           0        1.1            2.2        3.3
+      2          1           0        4.4            5.5        6.6
+      3          1           0        7.7            8.8        9.9
  ---------------------------------------------------------------------
 
 ...
 
- SCF Done:  E(RHF) =  -75.9834173665     A.U. after   10 cycles
-"""
+ SCF Done:  E(RB3LYP) =  -12.3456789     A.U. after    9 cycles
 
-BUF_H2O_L601 = r"""
- (Enter /opt/bwhpc/common/chem/gaussian/g16.C.01/x86_64-Intel-avx2-source/g16/l601.exe)
 ...
- Dipole moment (field-independent basis, Debye):
-    X=              0.0000    Y=             -0.0000    Z=             -2.6431  Tot=              2.6431
-"""  # noqa: E501
 
-BUF_H2O_L716 = r"""
- (Enter /opt/bwhpc/common/chem/gaussian/g16.C.01/x86_64-Intel-avx2-source/g16/l716.exe)
- Dipole        = 3.27065103D-16-1.33226763D-15-1.03989005D+00
  -------------------------------------------------------------------
  Center     Atomic                   Forces (Hartrees/Bohr)
  Number     Number              X              Y              Z
  -------------------------------------------------------------------
-      1        8          -0.000000000   -0.000000000   -0.036558637
-      2        1          -0.000000000   -0.003968101    0.018279318
-      3        1           0.000000000    0.003968101    0.018279318
+      1        8              0.1              0.2            0.3
+      2        1              0.4              0.5            0.6
+      3        1              0.7              0.8            0.9
  -------------------------------------------------------------------
-"""  # noqa: E501
+"""
 
 BUF_F2_RHF = r"""
  Entering Gaussian System, Link 0=g16
@@ -193,44 +184,26 @@ def test_match_magic():
     assert match_magic(bytebuf).name == 'gaussian-out'
 
 
-def test_gaussian_out_l601():
-    """Test if positions and energy are parsed correctly.
-
-    Test also if dipole moment is parsed correctly from `l601.exe`.
-    This corresponds to the options without `Forces` and `Pop=None`.
-    """
-    atoms = read(StringIO(BUF_H2O + BUF_H2O_L601), format='gaussian-out')
+def test_gaussian_out():
+    """Test if positions, energy, and forces are parsed correctly."""
+    atoms = read(StringIO(BUF_H2O), format='gaussian-out')
     assert str(atoms.symbols) == 'OH2'
     assert atoms.positions == pytest.approx(np.array([
-        [+0.000000, +0.000000, +0.119262],
-        [+0.000000, +0.763239, -0.477047],
-        [+0.000000, -0.763239, -0.477047],
+        [1.1, 2.2, 3.3],
+        [4.4, 5.5, 6.6],
+        [7.7, 8.8, 9.9],
     ]))
     assert not any(atoms.pbc)
     assert atoms.cell.rank == 0
 
     energy = atoms.get_potential_energy()
-    assert energy / units.Ha == pytest.approx(-75.9834173665)
-
-    dipole_moment_ref = pytest.approx(np.array([+0.0000, -0.0000, -2.6431]))
-    assert atoms.get_dipole_moment() / units.Debye == dipole_moment_ref
-
-
-def test_gaussian_out_l716():
-    """Test if forces and dipole moment are parsed correctly from `l716.exe`.
-
-    This corresponds to the options with `Forces` and `Pop=None`.
-    """
-    atoms = read(StringIO(BUF_H2O + BUF_H2O_L716), format='gaussian-out')
     forces = atoms.get_forces()
+    assert energy / units.Ha == pytest.approx(-12.3456789)
     assert forces / (units.Ha / units.Bohr) == pytest.approx(np.array([
-        [-0.000000000, -0.000000000, -0.036558637],
-        [-0.000000000, -0.003968101, +0.018279318],
-        [+0.000000000, +0.003968101, +0.018279318],
+        [0.1, 0.2, 0.3],
+        [0.4, 0.5, 0.6],
+        [0.7, 0.8, 0.9],
     ]))
-    assert atoms.get_dipole_moment() / units.Bohr == pytest.approx(np.array(
-        [+3.27065103e-16, -1.33226763e-15, -1.03989005e+00],
-    ))
 
 
 def test_mp2():
