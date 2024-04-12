588942652f86023f40177b75d8f2bdddd8a4ca08
Sebastian Echeverri Restrepo <sebastianecheverrir@gmail.com>
2024-04-12 17:32:44 +0000

    Implementation of FIRE2.0 and ABCFIRE

--- ase/optimize/fire2.py
+++ ase/optimize/fire2.py
@@ -1,44 +1,33 @@
-from typing import IO, Any, Callable, Dict, List, Optional, Union
+# ######################################
+# Implementation of FIRE2.0 and ABC-FIRE
+
+# The FIRE2.0 algorithm is implemented using the integrator euler semi implicit
+#  as described in the paper:
+#   J. Guénolé, W.G. Nöhring, A. Vaid, F. Houllé, Z. Xie, A. Prakash,
+#   E. Bitzek,
+#    Assessment and optimization of the fast inertial relaxation engine (fire)
+#    for energy minimization in atomistic simulations and its
+#    implementation in lammps,
+#    Comput. Mater. Sci. 175 (2020) 109584.
+#    https://doi.org/10.1016/j.commatsci.2020.109584.
+#    This implementation does not include N(p<0), initialdelay
+#
+# ABC-Fire is implemented as described in the paper:
+#   S. Echeverri Restrepo, P. Andric,
+#    ABC-FIRE: Accelerated Bias-Corrected Fast Inertial Relaxation Engine,
+#    Comput. Mater. Sci. 218 (2023) 111978.
+#    https://doi.org/10.1016/j.commatsci.2022.111978.
+#######################################
+
+from typing import IO, Callable, Optional, Union
 
 import numpy as np
 
 from ase import Atoms
 from ase.optimize.optimize import Optimizer
-from ase.utils import deprecated
 
 
-def _forbid_maxmove(args: List, kwargs: Dict[str, Any]) -> bool:
-    """Set maxstep with maxmove if not set."""
-    maxstep_index = 6
-    maxmove_index = 7
-
-    def _pop_arg(name: str) -> Any:
-        to_pop = None
-        if len(args) > maxmove_index:
-            to_pop = args[maxmove_index]
-            args[maxmove_index] = None
-
-        elif name in kwargs:
-            to_pop = kwargs[name]
-            del kwargs[name]
-        return to_pop
-
-    if len(args) > maxstep_index and args[maxstep_index] is None:
-        value = args[maxstep_index] = _pop_arg("maxmove")
-    elif kwargs.get("maxstep", None) is None:
-        value = kwargs["maxstep"] = _pop_arg("maxmove")
-    else:
-        return False
-
-    return value is not None
-
-
-class FIRE(Optimizer):
-    @deprecated(
-        "Use of `maxmove` is deprecated. Use `maxstep` instead.",
-        category=FutureWarning,
-        callback=_forbid_maxmove,
-    )
+class FIRE2(Optimizer):
     def __init__(
         self,
         atoms: Atoms,
@@ -46,19 +35,18 @@ class FIRE(Optimizer):
         logfile: Union[IO, str] = '-',
         trajectory: Optional[str] = None,
         dt: float = 0.1,
-        maxstep: Optional[float] = None,
-        maxmove: Optional[float] = None,
+        maxstep: float = 0.2,
         dtmax: float = 1.0,
-        Nmin: int = 5,
+        dtmin: float = 2e-3,
+        Nmin: int = 20,
         finc: float = 1.1,
         fdec: float = 0.5,
-        astart: float = 0.1,
+        astart: float = 0.25,
         fa: float = 0.99,
-        a: float = 0.1,
         master: Optional[bool] = None,
-        downhill_check: bool = False,
         position_reset_callback: Optional[Callable] = None,
         force_consistent=Optimizer._deprecated,
+        use_abc: Optional[bool] = False
     ):
         """Parameters:
 
@@ -82,15 +70,19 @@ class FIRE(Optimizer):
 
         maxstep: float
             Used to set the maximum distance an atom can move per
-            iteration (default value is 0.2).
+            iteration (default value is 0.2). Note that for ABC-FIRE the
+            check is done independently for each cartesian direction.
 
         dtmax: float
             Maximum time step. Default value is 1.0
 
+        dtmin: float
+            Minimum time step. Default value is 2e-3
+
         Nmin: int
             Number of steps to wait after the last time the dot product of
             the velocity and force is negative (P in The FIRE article) before
-            increasing the time step. Default value is 5.
+            increasing the time step. Default value is 20.
 
         finc: float
             Factor to increase the time step. Default value is 1.1
@@ -101,26 +93,14 @@ class FIRE(Optimizer):
         astart: float
             Initial value of the parameter a. a is the Coefficient for
             mixing the velocity and the force. Called alpha in the FIRE article.
-            Default value 0.1.
+            Default value 0.25.
 
         fa: float
             Factor to decrease the parameter alpha. Default value is 0.99
 
-        a: float
-            Coefficient for mixing the velocity and the force. Called
-            alpha in the FIRE article. Default value 0.1.
-
         master: boolean
             Defaults to None, which causes only rank 0 to save files.  If
             set to true,  this rank will save files.
-
-        downhill_check: boolean
-            Downhill check directly compares potential energies of subsequent
-            steps of the FIRE algorithm rather than relying on the current
-            product v*f that is positive if the FIRE dynamics moves downhill.
-            This can detect numerical issues where at large time steps the step
-            is uphill in energy even though locally v*f is positive, i.e. the
-            algorithm jumps over a valley because of a too large time step.
 
         position_reset_callback: function(atoms, r, e, e_last)
             Function that takes current *atoms* object, an array of position
@@ -133,9 +113,12 @@ class FIRE(Optimizer):
             force-consistent energies if available in the calculator, but
             falls back to force_consistent=False if not.
 
-        .. deprecated:: 3.19.3
-            Use of ``maxmove`` is deprecated; please use ``maxstep``.
-        """
+        use_abc: bool
+            If True, the Accelerated Bias-Corrected FIRE algorithm is
+            used (ABC-FIRE).
+            Default value is False.
+
+       """
         Optimizer.__init__(self, atoms, restart, logfile, trajectory,
                            master, force_consistent=force_consistent)
 
@@ -149,14 +132,15 @@ class FIRE(Optimizer):
             self.maxstep = self.defaults["maxstep"]
 
         self.dtmax = dtmax
+        self.dtmin = dtmin
         self.Nmin = Nmin
         self.finc = finc
         self.fdec = fdec
         self.astart = astart
         self.fa = fa
-        self.a = a
-        self.downhill_check = downhill_check
+        self.a = astart
         self.position_reset_callback = position_reset_callback
+        self.use_abc = use_abc
 
     def initialize(self):
         self.v = None
@@ -172,46 +156,65 @@ class FIRE(Optimizer):
 
         if self.v is None:
             self.v = np.zeros((len(optimizable), 3))
-            if self.downhill_check:
-                self.e_last = optimizable.get_potential_energy()
-                self.r_last = optimizable.get_positions().copy()
-                self.v_last = self.v.copy()
         else:
-            is_uphill = False
-            if self.downhill_check:
-                e = optimizable.get_potential_energy()
-                # Check if the energy actually decreased
-                if e > self.e_last:
-                    # If not, reset to old positions...
-                    if self.position_reset_callback is not None:
-                        self.position_reset_callback(
-                            optimizable, self.r_last, e,
-                            self.e_last)
-                    optimizable.set_positions(self.r_last)
-                    is_uphill = True
-                self.e_last = optimizable.get_potential_energy()
-                self.r_last = optimizable.get_positions().copy()
-                self.v_last = self.v.copy()
 
             vf = np.vdot(f, self.v)
-            if vf > 0.0 and not is_uphill:
-                self.v = (1.0 - self.a) * self.v + self.a * f / np.sqrt(
-                    np.vdot(f, f)) * np.sqrt(np.vdot(self.v, self.v))
+            if vf > 0.0:
+
+                self.Nsteps += 1
                 if self.Nsteps > self.Nmin:
                     self.dt = min(self.dt * self.finc, self.dtmax)
                     self.a *= self.fa
-                self.Nsteps += 1
             else:
-                self.v[:] *= 0.0
-                self.a = self.astart
-                self.dt *= self.fdec
                 self.Nsteps = 0
+                self.dt = max(self.dt * self.fdec, self.dtmin)
+                self.a = self.astart
 
+                dr = - 0.5 * self.dt * self.v
+                r = optimizable.get_positions()
+                optimizable.set_positions(r + dr)
+                self.v[:] *= 0.0
+
+        # euler semi implicit
+        f = optimizable.get_forces()
         self.v += self.dt * f
+
+        if self.use_abc:
+            self.a = max(self.a, 1e-10)
+            abc_multiplier = 1. / (1. - (1. - self.a)**(self.Nsteps + 1))
+            v_mix = ((1.0 - self.a) * self.v + self.a * f / np.sqrt(
+                np.vdot(f, f)) * np.sqrt(np.vdot(self.v, self.v)))
+            self.v = abc_multiplier * v_mix
+
+            # Verifying if the maximum distance an atom
+            #  moved is larger than maxstep, for ABC-FIRE the check
+            #  is done independently for each cartesian direction
+            if np.all(self.v):
+                v_tmp = []
+                for car_dir in range(3):
+                    v_i = np.where(np.abs(self.v[:, car_dir]) *
+                                   self.dt > self.maxstep,
+                                   (self.maxstep / self.dt) *
+                                   (self.v[:, car_dir] /
+                                   np.abs(self.v[:, car_dir])),
+                                   self.v[:, car_dir])
+                    v_tmp.append(v_i)
+                self.v = np.array(v_tmp).T
+
+        else:
+            self.v = ((1.0 - self.a) * self.v + self.a * f / np.sqrt(
+                np.vdot(f, f)) * np.sqrt(np.vdot(self.v, self.v)))
+
         dr = self.dt * self.v
-        normdr = np.sqrt(np.vdot(dr, dr))
-        if normdr > self.maxstep:
-            dr = self.maxstep * dr / normdr
+
+        # Verifying if the maximum distance an atom moved
+        #  step is larger than maxstep, for FIRE2.
+        if not self.use_abc:
+            normdr = np.sqrt(np.vdot(dr, dr))
+            if normdr > self.maxstep:
+                dr = self.maxstep * dr / normdr
+
         r = optimizable.get_positions()
         optimizable.set_positions(r + dr)
+
         self.dump((self.v, self.dt))
