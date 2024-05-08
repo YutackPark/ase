8c40e8c4a7bef6183da1df644db2d9194c888673
TomD <T.Demeyere@soton.ac.uk>
2024-05-08 07:39:37 +0100

    split tests

--- ase/test/atoms/test_center.py
+++ ase/test/atoms/test_center.py
@@ -119,25 +119,31 @@ def test_center_nocell():
     assert atoms.positions == pytest.approx(0)
 
 
-def test_center_about():
+def test_center_about1():
     atoms = Atoms('H', positions=[[1., 2., 3.]])
     atoms.center(vacuum=10.0, about=(0, 0, 0), axis=2)
     assert atoms.positions[0, 0] != pytest.approx(0)
     assert atoms.positions[0, 1] != pytest.approx(0)
     assert atoms.positions[0, 2] == pytest.approx(0)
 
+
+def test_center_about2():
     atoms = Atoms('H', positions=[[1., 2., 3.]])
     atoms.center(vacuum=10.0, about=(0, 5, 0), axis=(1, 2))
     assert atoms.positions[0, 0] != pytest.approx(0)
     assert atoms.positions[0, 1] == pytest.approx(5)
     assert atoms.positions[0, 2] == pytest.approx(0)
 
+
+def test_center_about3():
     atoms = Atoms('H', positions=[[1., 2., 3.]])
     atoms.center(vacuum=10.0, about=(10, 5, 2), axis=(0, 1, 2))
     assert atoms.positions[0, 0] == pytest.approx(10)
     assert atoms.positions[0, 1] == pytest.approx(5)
     assert atoms.positions[0, 2] == pytest.approx(2)
 
+
+def test_center_about4():
     atoms = Atoms('H', positions=[[1., 2., 3.]])
     atoms.center(vacuum=10.0, about=(0, 0, 0))
     assert atoms.positions[0, 0] == pytest.approx(0)
